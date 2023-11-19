4641e597564486188d42f69b49cb36b2ac92f1c2
Megha21337 <108426541+Megha21337@users.noreply.github.com>
2023-11-19 13:16:17 +0530

    Update myTopo-3sw-6Hosts.py

--- Custom-Topology/myTopo-3sw-6Hosts.py
+++ Custom-Topology/myTopo-3sw-6Hosts.py
@@ -1,1 +1,35 @@
+from mininet.topo import Topo
 
+class customTopo(Topo):
+
+  def create(self):
+    
+    #Adding hosts and switches
+    H1_S1 = self.addHost( 'h1' )
+    H2_S1 = self.addHost( 'h2' )
+    H3_S2 = self.addHost( 'h3' )
+    H4_S2 = self.addHost( 'h4' )
+    #H2_S2 = self.addHost( 'h2' )
+    H5_S3 = self.addHost( 'h5' )
+    #H2_S3 = self.addHost( 'h2' )
+    H6_S3 = self.addHost( 'h6' )
+
+    s1_Switch = self.addSwitch( 's1' )
+    s2_Switch = self.addSwitch( 's2' )
+    s3_Switch = self.addSwitch( 's3' )
+
+    # Add links
+    self.addLink( H1_S1, s1_Switch )
+    self.addLink( s2_Switch, s1_Switch )
+    self.addLink( H2_S1, s1_Switch )
+
+    self.addLink( H3_S2, s2_Switch )
+    self.addLink( s2_Switch, s3_Switch )
+    self.addLink( H4_S2, s2_Switch )
+    self.addLink( H2_S1, s2_Switch )
+
+    self.addLink( H5_S3, s3_Switch )
+    self.addLink( H6_S3, s3_Switch )
+    self.addLink( H2_S1, s3_Switch )
+   
+topos = { 'mytopo': ( lambda: customTopo() ) }
